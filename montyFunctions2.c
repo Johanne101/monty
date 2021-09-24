cdc4a14e06fa66d154f8aad49d59c62797ca46d1
johanne101 <jlopez0493@gmail.com>
2021-09-24 02:56:24 +0000

    [Add] add operand function

---
+++ montyFunctions2.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/*
+ * _add - Adds the top two elements of the stack.
+ * @head: first node of the list.
+ * @line: Is count of lines.
+ */
+void _add(stack_t **head, unsigned int line)
+{
+	int sum;
+
+	if ((*head == NULL) || ((*head)->next->next == NULL))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short", line);
+			exit(EXIT_FAILURE);
+	}
+	sum = (*head)->n;
+	(*head) = (*head)->next;
+	(*head)->n += sum;
+	(*head)->prev = NULL;
+
+}
